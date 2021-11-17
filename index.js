5997fb4bf75671daf0ec3d24d4f74876f78342b6
Yuri <yuri.shiba@unifesp.br>
2021-11-17 18:17:51 -0300

    Configured Docker

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World from Docker!');
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
